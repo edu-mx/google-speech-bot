591e9593442430077040a533f6611f628835c6dc
Eduardo Araújo <diaseduardo139@gmail.com>
2023-08-26 17:26:40 -0300

    chore: refactoring tests for the TTS function and get_token_bot.

--- test_main.py
+++ test_main.py
@@ -2,11 +2,12 @@ from main import get_token_bot, TTS
 def test_get_token_bot():
     file1 = get_token_bot()
     assert file1 != None
-    file2 = get_token_bot(filename='requirements.txt')
+    
+    file2 = get_token_bot('requirements.txt')
     assert file2 != None
 
 def test_TTS():
     audio_text = TTS()
     assert audio_text != None
     assert '.mp3' in audio_text
-    
+
