4204405d1ebc062585de84cf1f252940a4ac5763
Eduardo Araújo <diaseduardo139@gmail.com>
2023-08-25 22:10:08 -0300

    feat: included defaults in the TTS function for a text that works globally and language set to English, also added the teste of the same.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # diaseduardo139@gmail.com
 import os
+import logging as log
 from random import randint as rand
 import telebot
+from gtts import gTTS
+log.basicConfig(level=log.ERROR, filename='bot_log.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
 
 # token in token_bot.txt
 def get_token_bot(filename='token_bot.txt'):
@@ -9,12 +12,27 @@ def get_token_bot(filename='token_bot.txt'):
         with open(filename, 'r', encoding='utf-8') as file:
             token = file.read().strip()
             return token
-    except FileNotFoundError:
-        print('The token_bot.txt file does not exist, create it and add the bot token there;')
+    except FileNotFoundError as x:
+        log.error('Token not found %s' % (x))
+        exit('The token_bot.txt file does not exist, create it and add the bot token there;')
         return
     finally:
         file.close()
 
+def TTS(text_voice='Hello: 1, 2, 3.', lang_voice='en'):
+    try:
+        tts_voice = gTTS(text_voice, lang=lang_voice)
+        folder = 'files'
+        audioTempFile = f'{folder}/Audio_{rand(100, 999)}_{lang_voice}.mp3'
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        tts_voice.save(audioTempFile)
+        return audioTempFile
+    except Exception as x:
+        log.critical('Não foi possível salvar o arquivo %s' % (x))
+        return
+
 token_bot = get_token_bot()
-bot = telebot.TeleBot(token_bot)
-get_token_bot()
+if token_bot:
+    bot = telebot.TeleBot(token_bot)
+else: exit('The file does not have a token, please open token_bot.txt and check its integrity.')
--- test_main.py
+++ test_main.py
@@ -1,6 +1,12 @@
+from main import get_token_bot, TTS
 def test_get_token_bot():
-    from main import get_token_bot
     file1 = get_token_bot()
     assert file1 != None
     file2 = get_token_bot(filename='requirements.txt')
     assert file2 != None
+
+def test_TTS():
+    audio_text = TTS()
+    assert audio_text != None
+    assert '.mp3' in audio_text
+    
